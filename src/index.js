eb6f37bf7dc36a5bce62fdc560bfbea5bba850e7
linaspe <linas@lekiam.lt>
2024-06-08 11:53:30 +0300

    .

--- src/index.js
+++ src/index.js
@@ -8,8 +8,8 @@ if (process.env.NODE_ENV !== "production") {
 }
 const server = http.createServer(app);
 
-const PORT = process.env.PORT || 3003;
+const PORT =  3003;
 
-server.listen(PORT, () => {
+server.listen(PORT, '::', () => {
   logger.info(`Server running on port ${PORT}`);
 });
